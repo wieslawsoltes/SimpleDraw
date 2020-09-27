627aab0800562e847e4e422e73b480686340323a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-27 22:29:51 +0200

    Update AvaloniaRenderer.cs

--- SimpleDraw/Views/AvaloniaRenderer.cs
+++ SimpleDraw/Views/AvaloniaRenderer.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Avalonia;
-using Avalonia.Input;
 using Avalonia.Media;
 using Avalonia.Media.Immutable;
 using SimpleDraw.ViewModels;
