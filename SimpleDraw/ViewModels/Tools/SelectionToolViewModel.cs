5eaf2b3a5a8a8004ac56c1d8a296ddb468191e7a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-27 22:56:21 +0200

    Update SelectionToolViewModel.cs

--- SimpleDraw/ViewModels/Tools/SelectionToolViewModel.cs
+++ SimpleDraw/ViewModels/Tools/SelectionToolViewModel.cs
@@ -1,4 +1,8 @@
-﻿namespace SimpleDraw.ViewModels
+﻿using System;
+using System.Diagnostics;
+using SkiaSharp;
+
+namespace SimpleDraw.ViewModels
 {
     public class SelectionToolViewModel : ToolBaseViewModel
     {
@@ -13,7 +17,12 @@
             {
                 case State.None:
                     {
-                        // TODO:
+                        var result = HitTest(canvas, x, y);
+                        if (result != null)
+                        {
+                            // TODO:
+                            Debug.WriteLine($"{result}");
+                        }
                     }
                     break;
                 case State.Pressed:
@@ -47,7 +56,12 @@
             {
                 case State.None:
                     {
-                        // TODO:
+                        var result = HitTest(canvas, x, y);
+                        if (result != null)
+                        {
+                            // TODO:
+                            Debug.WriteLine($"{result}");
+                        }
                     }
                     break;
                 case State.Pressed:
@@ -56,6 +70,57 @@
                     }
                     break;
             }
+        }
+
+        public static SKRect ToSKRect(RectangleShapeViewModel rectangleShape)
+        {
+            var x = Math.Min(rectangleShape.TopLeft.X, rectangleShape.BottomRight.X);
+            var y = Math.Min(rectangleShape.TopLeft.Y, rectangleShape.BottomRight.Y);
+            var width = Math.Abs(rectangleShape.TopLeft.X - rectangleShape.BottomRight.X);
+            var height = Math.Abs(rectangleShape.TopLeft.Y - rectangleShape.BottomRight.Y);
+            return SKRect.Create((float)x, (float)y, (float)width, (float)height);
+        }
+
+        private ShapeBaseViewModel HitTest(CanvasViewModel canvas, double x, double y)
+        {
+            foreach (var shape in canvas.Shapes)
+            {
+                switch (shape)
+                {
+                    case LineShapeViewModel lineShape:
+                        {
+                            var p1 = new SKPoint((float)lineShape.Start.X, (float)lineShape.Start.Y);
+                            var p2 = new SKPoint((float)lineShape.End.X, (float)lineShape.End.Y);
+                            var path = new SKPath() { FillType = SKPathFillType.Winding };
+                            path.MoveTo(p1);
+                            path.LineTo(p2);
+                            var bounds = path.ComputeTightBounds();
+                            var contains = bounds.Contains((float)x, (float)y);
+                            //var contains = path.Contains((float)x, (float)y);
+                            if (contains)
+                            {
+                                return lineShape;
+                            }
+                        }
+                        break;
+                    case RectangleShapeViewModel rectangleShape:
+                        {
+                            var rect = ToSKRect(rectangleShape);
+                            var path = new SKPath() { FillType = SKPathFillType.Winding };
+                            path.AddRect(rect);
+                            var contains = path.Contains((float)x, (float)y);
+                            if (contains)
+                            {
+                                return rectangleShape;
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return null;
         }
     }
 }
