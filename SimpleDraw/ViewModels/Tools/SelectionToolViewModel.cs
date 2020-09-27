c590f2db9fc8fb67d34a185a4d13ae9e1054dc9c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-27 22:44:32 +0200

    Create SelectionToolViewModel.cs

---
+++ SimpleDraw/ViewModels/Tools/SelectionToolViewModel.cs
@@ -0,0 +1,61 @@
+﻿namespace SimpleDraw.ViewModels
+{
+    public class SelectionToolViewModel : ToolBaseViewModel
+    {
+        private enum State { None, Pressed }
+        private State _state = State.None;
+
+        public override string Name => "Selection";
+
+        public override void Pressed(CanvasViewModel canvas, double x, double y, ToolPointerType pointerType)
+        {
+            switch (_state)
+            {
+                case State.None:
+                    {
+                        // TODO:
+                    }
+                    break;
+                case State.Pressed:
+                    {
+                        // TODO:
+                    }
+                    break;
+            }
+        }
+
+        public override void Released(CanvasViewModel canvas, double x, double y, ToolPointerType pointerType)
+        {
+            switch (_state)
+            {
+                case State.None:
+                    {
+                        // TODO:
+                    }
+                    break;
+                case State.Pressed:
+                    {
+                        // TODO:
+                    }
+                    break;
+            }
+        }
+
+        public override void Moved(CanvasViewModel canvas, double x, double y, ToolPointerType pointerType)
+        {
+            switch (_state)
+            {
+                case State.None:
+                    {
+                        // TODO:
+                    }
+                    break;
+                case State.Pressed:
+                    {
+                        // TODO:
+                    }
+                    break;
+            }
+        }
+    }
+}
