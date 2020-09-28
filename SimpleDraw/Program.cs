6c9574ed22d7e6f202e25bb080abe21138326d24
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2020-09-28 12:30:46 +0000

    Add headless support

--- SimpleDraw/Program.cs
+++ SimpleDraw/Program.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Headless;
 using Avalonia.ReactiveUI;
+using Avalonia.Threading;
 
 namespace SimpleDraw
 {
     class Program
     {
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                if (args[0] == "--vnc")
+                {
+                     BuildAvaloniaApp().StartWithHeadlessVncPlatform(null, 5901, args, ShutdownMode.OnMainWindowClose);
+                }
+                else if (args[0] == "--headless")
+                {
+                     BuildAvaloniaApp().UseHeadless(true).StartWithClassicDesktopLifetime(args);
+                }
+            }
+            else
+            {
+                BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+            }
+        }
 
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
