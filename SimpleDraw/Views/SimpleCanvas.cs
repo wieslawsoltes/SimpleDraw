07b7efe3764833d181bb3480153028fd89be1fb6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-28 22:39:38 +0200

    Update SimpleCanvas.cs

--- SimpleDraw/Views/SimpleCanvas.cs
+++ SimpleDraw/Views/SimpleCanvas.cs
@@ -115,18 +115,20 @@ namespace SimpleDraw.Views
 
             switch (e.Key)
             {
+                case Key.X:
+                    {
+                        if (e.KeyModifiers == KeyModifiers.Control)
+                        {
+                            Cut(canvas);
+                            InvalidateVisual();
+                        }
+                    }
+                    break;
                 case Key.C:
                     {
                         if (e.KeyModifiers == KeyModifiers.Control)
                         {
-                            var shared = new Dictionary<ViewModelBase, ViewModelBase>();
-
-                            _copy.Clear();
-
-                            foreach (var item in canvas.Selected)
-                            {
-                                _copy.Add(item.Clone(shared));
-                            }
+                            Copy(canvas);
                         }
                     }
                     break;
@@ -134,41 +136,7 @@ namespace SimpleDraw.Views
                     {
                         if (e.KeyModifiers == KeyModifiers.Control)
                         {
-                            var shared = new Dictionary<ViewModelBase, ViewModelBase>();
-
-                            canvas.Selected.Clear();
-
-                            foreach (var item in _copy)
-                            {
-                                var clone = item.Clone(shared);
-                                canvas.Items.Add(clone);
-                                canvas.Selected.Add(clone);
-                            }
-
-                            InvalidateVisual();
-                        }
-                    }
-                    break;
-                case Key.X:
-                    {
-                        if (e.KeyModifiers == KeyModifiers.Control)
-                        {
-                            var shared = new Dictionary<ViewModelBase, ViewModelBase>();
-
-                            _copy.Clear();
-
-                            foreach (var item in canvas.Selected)
-                            {
-                                _copy.Add(item.Clone(shared));
-                            }
-
-                            foreach (var item in canvas.Selected)
-                            {
-                                canvas.Items.Remove(item);
-                            }
-
-                            canvas.Selected.Clear();
-
+                            Paste(canvas);
                             InvalidateVisual();
                         }
                     }
@@ -177,18 +145,131 @@ namespace SimpleDraw.Views
                     {
                         if (e.KeyModifiers == KeyModifiers.None)
                         {
-                            foreach (var item in canvas.Selected)
-                            {
-                                canvas.Items.Remove(item);
-                            }
-
-                            canvas.Selected.Clear();
-
+                            Delete(canvas);
                             InvalidateVisual();
                         }
                     }
                     break;
+                case Key.Escape:
+                    {
+                        if (e.KeyModifiers == KeyModifiers.None)
+                        {
+                            canvas.Selected.Clear();
+                        }
+                    }
+                    break;
+                case Key.A:
+                    {
+                        if (e.KeyModifiers == KeyModifiers.Control)
+                        {
+                            canvas.Selected.Clear();
+
+                            foreach (var item in canvas.Items)
+                            {
+                                canvas.Selected.Add(item);
+                            }
+                        }
+                    }
+                    break;
+                case Key.N:
+                    {
+                        if (e.KeyModifiers == KeyModifiers.None)
+                        {
+                            SetTool<NoneToolViewModel>(canvas);
+                        }
+                    }
+                    break;
+                case Key.S:
+                    {
+                        if (e.KeyModifiers == KeyModifiers.None)
+                        {
+                            SetTool<SelectionToolViewModel>(canvas);
+                        }
+                    }
+                    break;
+                case Key.L:
+                    {
+                        if (e.KeyModifiers == KeyModifiers.None)
+                        {
+                            SetTool<LineToolViewModel>(canvas);
+                        }
+                    }
+                    break;
+                case Key.R:
+                    {
+                        if (e.KeyModifiers == KeyModifiers.None)
+                        {
+                            SetTool<RectangleToolViewModel>(canvas);
+                        }
+                    }
+                    break;
             }
+        }
+
+        private void SetTool<T>(CanvasViewModel canvas) where T : ToolBaseViewModel
+        {
+            foreach (var tool in canvas.Tools)
+            {
+                if (tool is T toolType)
+                {
+                    canvas.Tool = toolType;
+                }
+            }
+        }
+
+        private void Cut(CanvasViewModel canvas)
+        {
+            var shared = new Dictionary<ViewModelBase, ViewModelBase>();
+
+            _copy.Clear();
+
+            foreach (var item in canvas.Selected)
+            {
+                _copy.Add(item.Clone(shared));
+            }
+
+            foreach (var item in canvas.Selected)
+            {
+                canvas.Items.Remove(item);
+            }
+
+            canvas.Selected.Clear();
+        }
+
+        private void Copy(CanvasViewModel canvas)
+        {
+            var shared = new Dictionary<ViewModelBase, ViewModelBase>();
+
+            _copy.Clear();
+
+            foreach (var item in canvas.Selected)
+            {
+                _copy.Add(item.Clone(shared));
+            }
+        }
+
+        private void Paste(CanvasViewModel canvas)
+        {
+            var shared = new Dictionary<ViewModelBase, ViewModelBase>();
+
+            canvas.Selected.Clear();
+
+            foreach (var item in _copy)
+            {
+                var clone = item.Clone(shared);
+                canvas.Items.Add(clone);
+                canvas.Selected.Add(clone);
+            }
+        }
+
+        private void Delete(CanvasViewModel canvas)
+        {
+            foreach (var item in canvas.Selected)
+            {
+                canvas.Items.Remove(item);
+            }
+
+            canvas.Selected.Clear();
         }
 
         public override void Render(DrawingContext context)
