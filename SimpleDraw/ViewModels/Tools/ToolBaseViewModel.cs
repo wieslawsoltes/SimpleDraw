c8cb41af27819115e234595e2f6d247c78cf87f8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-28 23:29:12 +0200

    Update ToolBaseViewModel.cs

--- SimpleDraw/ViewModels/Tools/ToolBaseViewModel.cs
+++ SimpleDraw/ViewModels/Tools/ToolBaseViewModel.cs
@@ -5,6 +5,7 @@ namespace SimpleDraw.ViewModels
     public abstract class ToolBaseViewModel : ViewModelBase
     {
         public abstract string Name { get; }
+
         public abstract void Pressed(CanvasViewModel canvas, double x, double y, ToolPointerType pointerType, ToolKeyModifiers keyModifiers);
         
         public abstract void Released(CanvasViewModel canvas, double x, double y, ToolPointerType pointerType, ToolKeyModifiers keyModifiers);
