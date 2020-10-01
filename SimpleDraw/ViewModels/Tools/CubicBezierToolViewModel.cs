36be4b88ff90ae5bccc5f9cf067008d33e429be4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-10-01 23:56:57 +0200

    Update CubicBezierToolViewModel.cs

--- SimpleDraw/ViewModels/Tools/CubicBezierToolViewModel.cs
+++ SimpleDraw/ViewModels/Tools/CubicBezierToolViewModel.cs
@@ -7,56 +7,13 @@ namespace SimpleDraw.ViewModels
     [DataContract(IsReference = true)]
     public class CubicBezierToolViewModel : ToolBaseViewModel
     {
-        private enum CubicBezierState { StartPoint, Point1, Point2, Point3 }
-        private CubicBezierState _state = CubicBezierState.StartPoint;
-        private CubicBezierShapeViewModel _cubicBezier;
-        private BrushViewModel _brush;
-        private PenViewModel _pen;
-        private double _hitRadius;
-        private bool _tryToConnect;
-        private bool _isStroked;
-        private bool _isFilled;
+        private ShapeToolViewModel _shapeTool;
 
         [DataMember(IsRequired = false, EmitDefaultValue = true)]
-        public BrushViewModel Brush
+        public ShapeToolViewModel ShapeTool
         {
-            get => _brush;
-            set => this.RaiseAndSetIfChanged(ref _brush, value);
-        }
-
-        [DataMember(IsRequired = false, EmitDefaultValue = true)]
-        public PenViewModel Pen
-        {
-            get => _pen;
-            set => this.RaiseAndSetIfChanged(ref _pen, value);
-        }
-
-        [DataMember(IsRequired = false, EmitDefaultValue = true)]
-        public bool IsStroked
-        {
-            get => _isStroked;
-            set => this.RaiseAndSetIfChanged(ref _isStroked, value);
-        }
-
-        [DataMember(IsRequired = false, EmitDefaultValue = true)]
-        public bool IsFilled
-        {
-            get => _isFilled;
-            set => this.RaiseAndSetIfChanged(ref _isFilled, value);
-        }
-
-        [DataMember(IsRequired = false, EmitDefaultValue = true)]
-        public double HitRadius
-        {
-            get => _hitRadius;
-            set => this.RaiseAndSetIfChanged(ref _hitRadius, value);
-        }
-
-        [DataMember(IsRequired = false, EmitDefaultValue = true)]
-        public bool TryToConnect
-        {
-            get => _tryToConnect;
-            set => this.RaiseAndSetIfChanged(ref _tryToConnect, value);
+            get => _shapeTool;
+            set => this.RaiseAndSetIfChanged(ref _shapeTool, value);
         }
 
         [IgnoreDataMember]
@@ -64,215 +21,17 @@ namespace SimpleDraw.ViewModels
 
         public override void Pressed(CanvasViewModel canvas, double x, double y, ToolPointerType pointerType, ToolKeyModifiers keyModifiers)
         {
-            switch (_state)
-            {
-                case CubicBezierState.StartPoint:
-                    {
-                        if (pointerType == ToolPointerType.Left)
-                        {
-                            var shared = new Dictionary<ViewModelBase, ViewModelBase>();
-                            var topLeft = default(PointViewModel);
-
-                            if (_tryToConnect)
-                            {
-                                var result = HitTest.Contains(canvas.Items, x, y, _hitRadius);
-                                if (result is PointViewModel point)
-                                {
-                                    topLeft = point;
-                                }
-                            }
-
-                            _cubicBezier = new CubicBezierShapeViewModel()
-                            {
-                                StartPoint = topLeft ?? new PointViewModel(x, y),
-                                Point1 = new PointViewModel(x, y),
-                                Point2 = new PointViewModel(x, y),
-                                Point3 = new PointViewModel(x, y),
-                                IsStroked = _isStroked,
-                                IsFilled = _isFilled,
-                                Brush = _brush.CloneSelf(shared),
-                                Pen = _pen.CloneSelf(shared)
-                            };
-                            canvas.Decorators.Add(_cubicBezier);
-                            canvas.Invalidate();
-                            _state = CubicBezierState.Point3;
-                        }
-                    }
-                    break;
-                case CubicBezierState.Point3:
-                    {
-                        if (pointerType == ToolPointerType.Left)
-                        {
-                            var point3 = default(PointViewModel);
-
-                            if (_tryToConnect)
-                            {
-                                var result = HitTest.Contains(canvas.Items, x, y, _hitRadius);
-                                if (result is PointViewModel point)
-                                {
-                                    point3 = point;
-                                }
-                            }
-
-                            if (point3 != null)
-                            {
-                                _cubicBezier.Point3 = point3;
-                            }
-
-                            canvas.Invalidate();
-
-                            _state = CubicBezierState.Point2;
-                        }
-
-                        if (pointerType == ToolPointerType.Right)
-                        {
-                            canvas.Decorators.Remove(_cubicBezier);
-                            canvas.Invalidate();
-                            _cubicBezier = null;
-                            _state = CubicBezierState.StartPoint;
-                        }
-                    }
-                    break;
-                case CubicBezierState.Point2:
-                    {
-                        if (pointerType == ToolPointerType.Left)
-                        {
-                            var point2 = default(PointViewModel);
-
-                            if (_tryToConnect)
-                            {
-                                var result = HitTest.Contains(canvas.Items, x, y, _hitRadius);
-                                if (result is PointViewModel point)
-                                {
-                                    point2 = point;
-                                }
-                            }
-
-                            if (point2 != null)
-                            {
-                                _cubicBezier.Point2 = point2;
-                            }
-
-                            canvas.Invalidate();
-
-                            _state = CubicBezierState.Point1;
-                        }
-
-                        if (pointerType == ToolPointerType.Right)
-                        {
-                            canvas.Decorators.Remove(_cubicBezier);
-                            canvas.Invalidate();
-                            _cubicBezier = null;
-                            _state = CubicBezierState.StartPoint;
-                        }
-                    }
-                    break;
-                case CubicBezierState.Point1:
-                    {
-                        if (pointerType == ToolPointerType.Left)
-                        {
-                            var point1 = default(PointViewModel);
-
-                            if (_tryToConnect)
-                            {
-                                var result = HitTest.Contains(canvas.Items, x, y, _hitRadius);
-                                if (result is PointViewModel point)
-                                {
-                                    point1 = point;
-                                }
-                            }
-
-                            if (point1 != null)
-                            {
-                                _cubicBezier.Point1 = point1;
-                            }
-
-                            canvas.Decorators.Remove(_cubicBezier);
-                            canvas.Items.Add(_cubicBezier);
-                            canvas.Invalidate();
-
-                            _cubicBezier = null;
-                            _state = CubicBezierState.StartPoint;
-                        }
-
-                        if (pointerType == ToolPointerType.Right)
-                        {
-                            canvas.Decorators.Remove(_cubicBezier);
-                            canvas.Invalidate();
-                            _cubicBezier = null;
-                            _state = CubicBezierState.StartPoint;
-                        }
-                    }
-                    break;
-            }
+            _shapeTool?.Pressed(canvas, x, y, pointerType, keyModifiers);
         }
 
         public override void Released(CanvasViewModel canvas, double x, double y, ToolPointerType pointerType, ToolKeyModifiers keyModifiers)
         {
-            switch (_state)
-            {
-                case CubicBezierState.StartPoint:
-                    {
-                    }
-                    break;
-                case CubicBezierState.Point3:
-                    {
-                    }
-                    break;
-                case CubicBezierState.Point1:
-                    {
-                    }
-                    break;
-                case CubicBezierState.Point2:
-                    {
-                    }
-                    break;
-            }
+            _shapeTool?.Released(canvas, x, y, pointerType, keyModifiers);
         }
 
         public override void Moved(CanvasViewModel canvas, double x, double y, ToolPointerType pointerType, ToolKeyModifiers keyModifiers)
         {
-            switch (_state)
-            {
-                case CubicBezierState.StartPoint:
-                    {
-                    }
-                    break;
-                case CubicBezierState.Point3:
-                    {
-                        if (pointerType == ToolPointerType.None)
-                        {
-                            _cubicBezier.Point2.X = x;
-                            _cubicBezier.Point2.Y = y;
-                            _cubicBezier.Point3.X = x;
-                            _cubicBezier.Point3.Y = y;
-                            canvas.Invalidate();
-                        }
-                    }
-                    break;
-                case CubicBezierState.Point2:
-                    {
-                        if (pointerType == ToolPointerType.None)
-                        {
-                            _cubicBezier.Point2.X = x;
-                            _cubicBezier.Point2.Y = y;
-                            _cubicBezier.Point1.X = x;
-                            _cubicBezier.Point1.Y = y;
-                            canvas.Invalidate();
-                        }
-                    }
-                    break;
-                case CubicBezierState.Point1:
-                    {
-                        if (pointerType == ToolPointerType.None)
-                        {
-                            _cubicBezier.Point1.X = x;
-                            _cubicBezier.Point1.Y = y;
-                            canvas.Invalidate();
-                        }
-                    }
-                    break;
-            }
+            _shapeTool?.Moved(canvas, x, y, pointerType, keyModifiers);
         }
 
         public override ToolBaseViewModel CloneSelf(Dictionary<ViewModelBase, ViewModelBase> shared)
@@ -284,10 +43,7 @@ namespace SimpleDraw.ViewModels
 
             var copy = new CubicBezierToolViewModel()
             {
-                Brush = _brush?.CloneSelf(shared),
-                Pen = _pen?.CloneSelf(shared),
-                IsStroked = _isStroked,
-                IsFilled = _isFilled
+                ShapeTool = _shapeTool?.CloneSelf(shared)
             };
 
             shared[this] = copy;
