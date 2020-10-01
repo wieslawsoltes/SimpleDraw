41b8cdd2da96dc16569adae12bff80a68ff3b0ae
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-10-01 19:28:29 +0200

    Create IItemsCanvas.cs

---
+++ SimpleDraw/ViewModels/Containers/IItemsCanvas.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SimpleDraw.ViewModels
+{
+    public interface IItemsCanvas
+    {
+        ObservableCollection<ViewModelBase> Items { get; set; }
+        ObservableCollection<ViewModelBase> Decorators { get; set; }
+        void Invalidate();
+    }
+}
