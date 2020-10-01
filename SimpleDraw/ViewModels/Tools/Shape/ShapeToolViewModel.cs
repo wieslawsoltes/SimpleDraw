0201d9afbe2368d127b5eed55578c68a033da07a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-10-01 23:56:35 +0200

    Create ShapeToolViewModel.cs

---
+++ SimpleDraw/ViewModels/Tools/Shape/ShapeToolViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace SimpleDraw.ViewModels
+{
+    [DataContract(IsReference = true)]
+    public abstract class ShapeToolViewModel : ViewModelBase
+    {
+        public abstract void Pressed(IItemsCanvas canvas, double x, double y, ToolPointerType pointerType, ToolKeyModifiers keyModifiers);
+
+        public abstract void Released(IItemsCanvas canvas, double x, double y, ToolPointerType pointerType, ToolKeyModifiers keyModifiers);
+
+        public abstract void Moved(IItemsCanvas canvas, double x, double y, ToolPointerType pointerType, ToolKeyModifiers keyModifiers);
+
+        public abstract ShapeToolViewModel CloneSelf(Dictionary<ViewModelBase, ViewModelBase> shared);
+    }
+}
