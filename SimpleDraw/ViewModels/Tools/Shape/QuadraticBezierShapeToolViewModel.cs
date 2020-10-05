76e3458f61098b6b6955ddf65575b47342bbc878
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-10-05 21:36:39 +0200

    Update QuadraticBezierShapeToolViewModel.cs

--- SimpleDraw/ViewModels/Tools/Shape/QuadraticBezierShapeToolViewModel.cs
+++ SimpleDraw/ViewModels/Tools/Shape/QuadraticBezierShapeToolViewModel.cs
@@ -71,9 +71,9 @@ namespace SimpleDraw.ViewModels.Tools.Shape
             if (_tryToConnect)
             {
                 var result = SkiaHitTest.Contains(canvas.Items, x, y, _hitRadius);
-                if (result is PointViewModel point)
+                if (result != null)
                 {
-                    canvas.Hovered.Add(point);
+                    canvas.Hovered.Add(result);
                 }
             }
         }
