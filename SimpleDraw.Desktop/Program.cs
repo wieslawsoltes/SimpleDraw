8cd995f2a1a9fea76ae645cad1d784c8587b4efd
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-02-04 22:30:14 +0100

    Refactor

--- SimpleDraw.Desktop/Program.cs
+++ SimpleDraw.Desktop/Program.cs
@@ -1,44 +1,22 @@
 ﻿using System;
 using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Dialogs;
-using Avalonia.Headless;
 using Avalonia.ReactiveUI;
 
-namespace SimpleDraw
+namespace SimpleDraw.Desktop
 {
     class Program
     {
         [STAThread]
         public static void Main(string[] args)
         {
-            if (args.Length == 1)
-            {
-                if (args[0] == "--vnc")
-                {
-                    BuildAvaloniaApp().StartWithHeadlessVncPlatform(null, 5901, args, ShutdownMode.OnMainWindowClose);
-                }
-                else if (args[0] == "--headless")
-                {
-                    BuildAvaloniaApp().UseHeadless(true).StartWithClassicDesktopLifetime(args);
-                }
-            }
-            else
-            {
-                BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
-            }
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
         }
 
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .LogToTrace()
-                .UseManagedSystemDialogs()
                 .UseSkia()
-                .With(new Win32PlatformOptions()
-                { 
-                    AllowEglInitialization = true 
-                })
-                .UseReactiveUI();
+                .UseReactiveUI()
+                .LogToTrace();
     }
 }
